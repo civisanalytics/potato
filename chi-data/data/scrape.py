c0ff5c25be4a56e5905da4ba03f96b812bb02499
cjhin <cjhin@civisanalytics.com>
2014-08-23 04:56:45 +0000

    data + scraper

---
+++ chi-data/data/scrape.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+import string
+
+print 'name,team,position,school'
+
+base_url = 'http://espn.go.com/nfl/college/_/letter/'
+
+for letter in string.lowercase:
+    html = urlopen(base_url + letter).read()
+    soup = BeautifulSoup(html)
+    section = soup.find(id="my-players-table")
+    table = section.table
+
+    college = ''
+
+    for row in table.find_all('tr'):
+        if 'stathead' in row['class']:
+            # new college
+            college = row.string
+        elif ('oddrow' in row['class'] or 'evenrow' in row['class']):
+            player = []
+            for col in row.find_all('td'):
+                #if col.string == None: #wtf?
+                #  col.string = 'none'
+                try:
+                    player.append(str(col.string))
+                except:
+                    player.append('')
+            if len(player) > 0: # if this row had anything
+                if 'No active players.' not in player:
+                    player.append(college)
+                    print str(','.join(player))
